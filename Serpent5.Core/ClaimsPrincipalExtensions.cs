d2ad3902c2c9e842a7a6820bd879cc8ac80d384e
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2022-08-16 17:00:07 +0100

    Add ClaimsPrincipalExtensions.GetNameIdentifier (#2)
    +semver: minor

---
+++ Serpent5.Core/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,29 @@
+using JetBrains.Annotations;
+
+// ReSharper disable once CheckNamespace
+namespace System.Security.Claims;
+
+/// <summary>
+/// Extensions for <see cref="ClaimsPrincipal" />.
+/// </summary>
+[PublicAPI]
+public static class ClaimsPrincipalExtensions
+{
+    /// <summary>
+    /// Returns the value of the <see cref="ClaimTypes.NameIdentifier" /> claim.
+    /// </summary>
+    /// <param name="claimsPrincipal">The <see cref="ClaimsPrincipal" /> to check for the claim.</param>
+    /// <returns>The value of the <see cref="ClaimTypes.NameIdentifier" /> claim.</returns>
+    /// <exception cref="InvalidOperationException">The <see cref="ClaimTypes.NameIdentifier" /> claim doesn't exist.</exception>
+    public static string GetNameIdentifier(this ClaimsPrincipal claimsPrincipal)
+    {
+        ArgumentNullException.ThrowIfNull(claimsPrincipal);
+
+        var nameIdentifier = claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (string.IsNullOrWhiteSpace(nameIdentifier))
+            throw new InvalidOperationException($"The \"{ClaimTypes.NameIdentifier}\" claim wasn't found.");
+
+        return nameIdentifier;
+    }
+}
