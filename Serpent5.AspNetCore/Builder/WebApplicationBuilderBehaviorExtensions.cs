bb04e531b10c6fd1882e66d8d84887b45edc4cd1
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2023-04-13 13:13:22 +0100

    Add overload for ConfigureBehavior

--- Serpent5.AspNetCore/Builder/WebApplicationBuilderBehaviorExtensions.cs
+++ Serpent5.AspNetCore/Builder/WebApplicationBuilderBehaviorExtensions.cs
@@ -21,11 +21,33 @@ public static class WebApplicationBuilderBehaviorExtensions
         string appName,
         Action<IWebApplicationBehaviorBuilder>? configureBehaviorBuilder = null)
     {
+        return webApplicationBuilder.ConfigureBehavior(appName, (webApplicationBehaviorBuilder, _) =>
+        {
+            configureBehaviorBuilder?.Invoke(webApplicationBehaviorBuilder);
+        });
+    }
+
+    /// <summary>
+    /// Configures a <see cref="WebApplicationBuilder" /> to use Behaviors.
+    /// </summary>
+    /// <param name="webApplicationBuilder">The <see cref="WebApplicationBuilder" /> to configure.</param>
+    /// <param name="appName">The name of the app, as shown in e.g. Application Insights.</param>
+    /// <param name="configureBehaviorBuilder">The action used to configure one or more Behaviors.</param>
+    /// <returns>A reference to <paramref name="webApplicationBuilder" /> for a fluent API.</returns>
+    /// <remarks>
+    /// A Behavior is a set of service-registrations and/or configuration that sets up a <see cref="WebApplicationBuilder" /> to behave as e.g. a Web API.
+    /// </remarks>
+    public static WebApplicationBuilder ConfigureBehavior(
+        this WebApplicationBuilder webApplicationBuilder,
+        string appName,
+        Action<IWebApplicationBehaviorBuilder, WebApplicationBuilder>? configureBehaviorBuilder = null)
+    {
         ArgumentNullException.ThrowIfNull(webApplicationBuilder);
+        ArgumentExceptionExtensions.ThrowIfNullOrWhiteSpace(appName);
 
         var webApplicationBehaviorBuilder = new WebApplicationBehaviorBuilder(appName);
 
-        configureBehaviorBuilder?.Invoke(webApplicationBehaviorBuilder);
+        configureBehaviorBuilder?.Invoke(webApplicationBehaviorBuilder, webApplicationBuilder);
 
         webApplicationBehaviorBuilder.Configure(webApplicationBuilder);
 
